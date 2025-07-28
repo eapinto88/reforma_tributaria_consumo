d74315731321fb31cbc2e0a2412ccc47a163c5fb
eapinto88 <everton.assis096@gmail.com>
2025-07-28 10:37:58 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 
-document.getElementById('root').innerHTML = `
-  <h1 style='text-align:center;margin-top:2rem;'>App Reforma Tributária - Time Fiscal</h1>
-  <p style='text-align:center;'>Instalável como app no celular</p>
-`;
+import App from './App.js';
+
+document.getElementById('root').innerHTML = '<h1>React App Placeholder</h1>';
