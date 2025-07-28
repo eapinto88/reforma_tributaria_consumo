d6bc44ef256efd56cba6bd1172418a152b81d3d6
eapinto88 <everton.assis096@gmail.com>
2025-07-28 10:02:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+
+document.getElementById('root').innerHTML = `
+  <h1 style='text-align:center;margin-top:2rem;'>App Reforma Tributária - Time Fiscal</h1>
+  <p style='text-align:center;'>Instalável como app no celular</p>
+`;
